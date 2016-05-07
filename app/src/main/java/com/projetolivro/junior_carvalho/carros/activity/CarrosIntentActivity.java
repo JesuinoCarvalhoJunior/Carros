ff08e8b4326a74ffdd41dddbf196f7e10e1ac6d0
JesuinoCarvalhoJunior <snakee_snakee@hotmail.com>
2016-05-07 18:36:14 -0400

    nova activity

---
+++ app/src/main/java/com/projetolivro/junior_carvalho/carros/activity/CarrosIntentActivity.java
@@ -0,0 +1,15 @@
+package com.projetolivro.junior_carvalho.carros.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.projetolivro.junior_carvalho.carros.R;
+
+public class CarrosIntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carros_intent);
+    }
+}
